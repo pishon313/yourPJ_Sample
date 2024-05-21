7475678cc0ba22123023c396a87f36ae77357d7a
Sarah Jeong <pishon313@gmail.com>
2024-05-21 10:32:46 +0900

    Initial Commit

---
+++ yourPJ_Sample/yourPJ_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  yourPJ_SampleApp.swift
+//  yourPJ_Sample
+//
+//  Created by Sarah Jeong on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct yourPJ_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
